0ffd331218345964a3a6e0eebd2b6fd36860847a
Wisoft <wisoft33@gmail.com>
2026-03-23 01:32:43 +0300

    ...app...

--- app/src/main/java/com/example/app/RegisterActivity.kt
+++ app/src/main/java/com/example/app/RegisterActivity.kt
@@ -2,16 +2,27 @@ package com.example.app
 //https://justpaste.it/jn3sy
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var edtEmail: EditText
+
+    lateinit var edtPass: EditText
+
+    lateinit var edtPass2: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+        edtEmail = findViewById(R.id.edtEmail)
+        edtPass = findViewById(R.id.edtPass)
+        edtPass2 = findViewById(R.id.edtPass2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
