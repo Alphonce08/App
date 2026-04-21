c7d412be84a6ee16155ca615d8d46d8d810d09c5
Wisoft <wisoft33@gmail.com>
2026-04-22 01:00:54 +0300

    ...app...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.11.0")
 }
